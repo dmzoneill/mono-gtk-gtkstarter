807980fad92e74bc3aea637d5dfa71e1a541dfee
David O Neill <dmz.oneill@gmail.com>
2009-11-28 16:42:04 +0000

    Init

---
+++ GtkStarter.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : GtkStarter.cpp
+// Author      : David O Neill
+// Version     :
+// Copyright   : feeditout.com
+// Description : Hello World in C, Ansi-style
+//============================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <gtkmm.h>
+
+int main(void) {
+	puts("fair play");
+	return EXIT_SUCCESS;
+}
